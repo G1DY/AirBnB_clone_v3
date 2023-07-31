abe8b6967a07c2f6a7e1ec112a4f58d45e59a899
Gideon <kipkerring.gedion@gmail.com>
2023-07-31 08:39:05 -0700

    app.py

---
+++ api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""creates variable app, instance of Flask"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+
+# Create a Flask app
+app = Flask(__name__)
+
+# Register the blueprint
+app.register_blueprint(app_views)
+
+
+# Method to handle teardown_appcontext
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    import os
+
+    # Get the host and port from environment variables or use default values
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", "5000"))
+
+    # Run the Flask server
+    app.run(host=host, port=port, threaded=True)
